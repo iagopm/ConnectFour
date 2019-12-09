6aadbccfe42574fafdf956025b80a4302ffe9c4c
iago <example@example.com>
2019-12-09 02:54:15 +0100

    animation to be implemented

--- src/main/java/animationLogic/AnimationFacadeImpl.java
+++ src/main/java/animationLogic/AnimationFacadeImpl.java
@@ -5,15 +5,33 @@ import model.Chip;
 
 public class AnimationFacadeImpl implements AnimationFacade {
     public GameApplication application;
-
     public AnimationFacadeImpl(GameApplication application) {
         this.application = application;
     }
-
     public void animate(Chip chip) {
-
-
+//        Bounds boundsInScene = chip.localToScene(chip.getBoundsInLocal());
+//        TranslateTransition translateTransition = new TranslateTransition(new Duration(1000), chip);
+//        System.out.println("" + boundsInScene.getMinX() + boundsInScene.getMinY());
+//        Bounds startBounds = application
+//                .getBoardFacade()
+//                .getColumn(
+//                        chip.getColumn())
+//                .get(0)
+//                .localToScene(
+//                        application
+//                                .getBoardFacade()
+//                                .getColumn(chip.getColumn())
+//                                .get(0).getBoundsInLocal()
+//                );
+//
+//        System.out.println("" + startBounds.getMinX() + startBounds.getMinX());
+//
+//        translateTransition.setFromY(startBounds.getMinY());
+//        translateTransition.setFromX(boundsInScene.getMinY());
+//
+//        translateTransition.setToX(boundsInScene.getMinX());
+//        translateTransition.setToY(boundsInScene.getMinY());
+//
+//        translateTransition.play();
     }
-
-
 }
